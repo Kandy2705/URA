7eda857e885b3a91eee3f372a0dc85a52570d7b6
EricDuong5423 <tuduong05042003@gmail.com>
2025-09-11 00:02:19 +0700

    fix: jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,5 +1,5 @@
 def PROJECT_NAME = "URA"
-def CUSTOME_WORKSPACE = "C:\\Project\\${PROJECT_NAME}"
+def CUSTOME_WORKSPACE = "C:\\Jenkins\\Unity_Projects\\${PROJECT_NAME}"
 def UNITY_VERSION = "6000.0.44f1"
 def UNITY_INSTALLATION = "C:\\Program Files\\Unity\\Hub\\Editor\\${UNITY_VERSION}\\Editor"
 pipeline{
@@ -13,6 +13,8 @@ pipeline{
             customWorkspace "${CUSTOME_WORKSPACE}"
         }
     }
+
+    options { skipDefaultCheckout(true) }
 
     stages{
         stage('Checkout') {
