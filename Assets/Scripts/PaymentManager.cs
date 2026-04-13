f6a9743b39980c96ad07cb6d1e5ef21effd5e80e
OtisHuang-new <khoihoang862006@gmail.com>
2026-04-13 23:11:16 +0700

    feat: add randomMistake method

--- Assets/Scripts/PaymentManager.cs
+++ Assets/Scripts/PaymentManager.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
-using TMPro; 
+using TMPro;
+using System;
+using System.Linq;
 
 public class PaymentManager : MonoBehaviour
 {
@@ -10,8 +12,8 @@ public class PaymentManager : MonoBehaviour
     private int currentAmount = 0;     
 
     [Header("Liên kết UI")]
-    public TMP_Text requiredAmountText; 
-    public TMP_Text currentAmountText; 
+    public TMP_Text requiredAmountText;
+    public TMP_Text currentAmountText;
 
     private void Awake()
     {
@@ -55,5 +57,117 @@ public class PaymentManager : MonoBehaviour
         {
             int missing = requiredAmount - currentAmount;
         }
+    }
+    public string randomMistake(double originalPayment)
+    {
+        const double MISTAKE_RATE = 0.1;
+        const int PAYMENT_TOLERANT = 50;
+
+        bool isMistaken = (UnityEngine.Random.value < MISTAKE_RATE) ? true : false;
+        if (isMistaken)
+        {
+            int randomTolerant = UnityEngine.Random.Range(0, PAYMENT_TOLERANT);
+            int newPayment = (int) (originalPayment * (1 + randomTolerant / 100.0));
+            Debug.Log("Số tiền bằng số  là " + newPayment);
+            Debug.Log("Số tiền bằng chữ là " + NumberToVietnamese(newPayment));
+            return "Số tiền thanh toán là " + NumberToVietnamese(newPayment) + "đồng";
+        }
+        else
+        {
+            return "Số tiền thanh toán là " + NumberToVietnamese((int) originalPayment) + "đồng";
+        }
+    }
+    
+   
+
+    private enum SpecialCase { MƯƠI, MƯỜI, LẺ }
+
+    private string HundredNumberToVietnamese(int number)
+    {
+        if (number == 0) return "Không";
+
+        string[] units = { "không", "một", "hai", "ba", "bốn", "năm", "sáu", "bảy", "tám", "chín" };
+        string result = "";
+
+        int tram = (number % 1000) / 100;
+        int muoi = (number % 100) / 10;
+        int donVi = number % 10;
+        bool hasNumber = false;
+        // Hàng Trăm
+        if (tram > 0) {
+            result += units[tram] + " trăm ";
+            hasNumber = true;
+        } 
+
+        // Hàng Chục
+        SpecialCase sCase = SpecialCase.MƯƠI;
+        if (muoi == 0)
+        {
+            if (donVi > 0 && hasNumber)
+            {
+                result += " lẻ ";
+                sCase = SpecialCase.LẺ;
+            }
+        }
+        else if (muoi == 1)
+        {
+            result += " mười ";
+            sCase = SpecialCase.MƯỜI;
+            hasNumber = true;
+        }
+        else
+        {
+            result += units[muoi] + " mươi ";
+            sCase = SpecialCase.MƯƠI;
+            hasNumber = true;
+        }
+
+        // Hàng Đơn vị
+        if (donVi > 0)
+        {
+            if (hasNumber && donVi == 1 && sCase == SpecialCase.MƯƠI) result += "mốt";
+            else if (hasNumber && donVi == 5 && sCase != SpecialCase.LẺ) result += "lăm";
+            else result += units[donVi];
+        }
+
+        return result.Trim().Replace("  ", " ");
+    }
+
+    private string NumberToVietnamese(int number)
+    {
+        if (number == 0) return "Không";
+        
+        string[] placeTxt = { "", " nghìn ", " triệu ", " tỷ " };
+        int[] placeNum = { 1, 1000, 1000000, 1000000000 };
+        string res = "";
+
+        int currentPlaceNum = 0;
+        for (int i = placeNum.Length - 1; i >= 0; i--)
+        {
+            if (number >= placeNum[i])
+            {
+                currentPlaceNum = i;
+                break;
+            }
+        }
+
+        while (currentPlaceNum >= 0)
+        {
+            int chunk = number / placeNum[currentPlaceNum];
+            
+            if (chunk > 0) 
+            {
+                res += HundredNumberToVietnamese(chunk) + placeTxt[currentPlaceNum];
+            }
+            else if (res != "" && currentPlaceNum > 0)
+            {
+                res += "không" + placeTxt[currentPlaceNum];
+            }
+
+            number %= placeNum[currentPlaceNum];
+            currentPlaceNum--;
+        }
+
+        return res.Trim();
     }
 }
